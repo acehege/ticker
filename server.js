34c418213deaaacf4929e04f1c5f238994444927
acehege <hjemmesideregistrering@gmail.com>
2015-06-05 14:22:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,9 @@ cp.on('message', function (message) {
 
 io.sockets.on('connection', function (socket) {
     socket.emit('status', { message: "Connected" });
-// start --> ticker.js " if (msg.op === 'startNOW') { ticker.execute();} " the function in the ticker.js listens for the op: msg "
+// start --> ticker.js " if (msg.op === 'startNOW') { ticker.execute();} " the function in the ticker.js listens for the op: msg " -->
+// the first value in ( socket.on ) is from the main.js, "$scope.start = function () {socket.emit('start_from_mainjs');};"
+// --> You can change the socket.emit XXX to whatever you want, just remember to change the value in ticker.js and main.js
     socket.on('start_from_mainjs', function (data) {
 // You can change the op: XXX to whatever you want, just remember to change the function in ticker.js
         cp.send({ op: 'startNOW' });
