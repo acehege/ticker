1c2a6ab73bcfe9127a4461dc0486d06eda982d3b
acehege <hjemmesideregistrering@gmail.com>
2015-06-07 13:43:01 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var jade = require('jade'),
 	app = express(),
     server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
-var PythonShell = require('python-shell');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 
